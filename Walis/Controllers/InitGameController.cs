4573125b834cd1392dbf56487f2b4c6e531589a1
shortpoet <soriano.carlitos@gmail.com>
2019-10-24 14:35:27 -0400

    added solution files

---
+++ Walis/Controllers/InitGameController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Walis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InitGameController : ControllerBase
+    {
+        // GET: api/InitGame
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/InitGame/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/InitGame
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/InitGame/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
